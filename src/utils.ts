1e2d7371581d916bc9be167df52eb49fabfd0d13
Fabio Spampinato <spampinabio@gmail.com>
2022-07-25 12:59:02 +0200

    Reverted toLowerCase optimization, as it barely moves the needle

--- src/utils.ts
+++ src/utils.ts
@@ -41,7 +41,7 @@ const isElementAction = ( value: Element ): value is HTMLFormElement => {
 
 const isElementIframe = ( value: Element ): value is HTMLIFrameElement => {
 
-  return ( value.tagName === 'IFRAME' );
+  return ( value.tagName.toLowerCase () === 'iframe' );
 
 };
 
